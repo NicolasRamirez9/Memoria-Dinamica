ede2b410ba57c56132fcfe0f9e020f4f03c464a3
Nico Ramirez <40794868+NicolasRamirez9@users.noreply.github.com>
2019-05-14 22:09:10 -0300

    Add files via upload

---
+++ memoria/empleado.c
@@ -0,0 +1,157 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "empleado.h"
+
+int getString(char* msg,char* msgE,int minimo,int maximo,int reintentos,char* resultado)
+{
+    int ret = -1;
+    char bufferStr[400];
+    if(msg != NULL && msgE != NULL && resultado != NULL && reintentos >=0 && maximo > minimo)
+    {
+        do
+        {
+            printf("\n%s",msg);
+            fflush(stdin);
+            fgets(bufferStr,sizeof(bufferStr),stdin);
+            bufferStr[strlen(bufferStr)-1] = '\0';
+            if(strlen(bufferStr) >= minimo && strlen(bufferStr)<maximo)
+            {
+                strncpy(resultado,bufferStr,maximo);
+                ret = 0;
+                break;
+            }
+            else
+            {
+                printf("\n%s",msgE);
+            }
+            reintentos --;
+        }
+        while(reintentos>0);
+    }
+    return ret;
+}
+
+
+Empleado* Emp_new(void)
+{
+    return (Empleado*) malloc(sizeof(Empleado));
+}
+
+int Emp_delete(Empleado* this)
+{
+    int retorno = -1;
+    if(this != NULL)
+    {
+        free(this);
+        retorno = 0;
+    }
+    return retorno;
+}
+
+int Emp_setId(Empleado* this, int id)
+{
+    int retorno = -1;
+    if(this != NULL && id >= 0)
+    {
+        this->id = id;
+        retorno = 0;
+    }
+    return retorno;
+}
+
+int Emp_getId(Empleado* this, int* resultado)
+{
+    int retorno = -1;
+    if(this != NULL && resultado != NULL)
+    {
+        *resultado = this->id;
+        retorno = 0;
+    }
+    return retorno;
+}
+
+int Emp_setNombre(Empleado* this, char* nombre)
+{
+    int retorno = -1;
+    if(this != NULL && nombre >= 0)
+    {
+        strcpy(this->nombre,nombre);
+        retorno = 0;
+    }
+    return retorno;
+}
+
+int Emp_getNombre(Empleado* this, char* resultado)
+{
+    int retorno = -1;
+    if(this != NULL && resultado != NULL)
+    {
+        strcpy(this->nombre,resultado);
+        retorno = 0;
+    }
+    return retorno;
+}
+
+int Emp_setPeso(Empleado* this, float peso)
+{
+    int retorno = -1;
+    if(this != NULL && peso >= 0)
+    {
+        this->peso = peso;
+        retorno = 0;
+    }
+    return retorno;
+}
+
+int Emp_getPeso(Empleado* this, float* resultado)
+{
+    int retorno = -1;
+    if(this != NULL && resultado != NULL)
+    {
+        *resultado = this->peso;
+        retorno = 0;
+    }
+    return retorno;
+}
+
+int Emp_setEstado(Empleado* this, int estado)
+{
+    int retorno = -1;
+    if(this != NULL && estado >= 0)
+    {
+        this->estado = estado;
+        retorno = 0;
+    }
+    return retorno;
+}
+
+int Emp_getEstado(Empleado* this, int* resultado)
+{
+    int retorno = -1;
+    if(this != NULL && resultado != NULL)
+    {
+        *resultado = this->estado;
+        retorno = 0;
+    }
+    return retorno;
+}
+
+int isValidName(char* stringRecibido)   //si fuera un numero podrìa necesitar parametros para validar max y min
+{
+    int retorno=1;  // para las funciones isValid arranco con verdadero y cambio cuando encuentro un error
+    int i;
+    for(i=0;stringRecibido[i]!='\0';i++)
+    {
+        //printf("%d",i);
+        if(stringRecibido[i]<'A' || (stringRecibido[i]>'Z' && stringRecibido[i]<'a') || stringRecibido[i]>'z')// o ((stringRecibido[i]<'A' || (stringRecibido[i]>'Z') && (stringRecibido[i]<'a' || stringRecibido[i]>'z'))
+        {
+            retorno=0;
+            break;
+        }
+    }
+    return retorno;
+}
+
+
+
